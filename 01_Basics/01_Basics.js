af0bfb0aff8f9eb83d6c0270693d1656bcaf7548
AnjalSotang <137472810+AnjalSotang@users.noreply.github.com>
2025-08-22 07:42:12 +0000

    DataTypes

---
+++ 01_Basics/01_Basics.js
@@ -0,0 +1,18 @@
+const accountId = 1445;
+let accountEmail = "anjal@gmail.com"
+var accountPassword = "12345"
+accountCity =" Khanar";
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "rai@gmail.com"
+accountPassword = "78783"
+accountCity = "Dharan"
+
+console.log(accountId);
+/*
+Prefere not to use var in js deu to blog and function scope problem
+*/
+
+//memory reservered
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_Basics/01_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3);// code readability should me must
+
+// for documentation mostly prefered is mdn but its not the original one its from modzilla but
+// to get the og one tc39.es == this is ecma script so 
+// So paila paila starting days ma harek broswer ma harek standard came so this echma
+
+let name = 'hitesh'
+let age = 198
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined);
+
+
